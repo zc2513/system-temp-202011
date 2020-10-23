f3b188602677da0290e9637205484567bebec2d5
zc2513 <yyzc2513@163.com>
2020-10-23 17:10:29 +0800

    添加白名单

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import path from 'path'
 import SvgIcon from '@/components/SvgIcon'
 
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
+
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+
+// svg名称集合
+export const svgNames = req.keys().map(e => path.basename(e, '.svg'))
