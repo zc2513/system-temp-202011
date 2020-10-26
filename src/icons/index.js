afc314852164dc87d90b6dfb55a1c5f1acda633f
zc2513 <yyzc2513@163.com>
2020-10-26 12:26:35 +0800

    添加路由处理部分内容

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import path from 'path'
 import SvgIcon from '@/components/SvgIcon'
 
 Vue.component('svg-icon', SvgIcon)
@@ -6,3 +7,7 @@ Vue.component('svg-icon', SvgIcon)
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+
+// svg名称集合
+export const svgNames = req.keys().map(e => path.basename(e, '.svg'))
+
--- src/views/system/pageRoute/index.vue
+++ src/views/system/pageRoute/index.vue
@@ -1,97 +1,29 @@
 
 <template>
   <div class="list-box">
-    路由维护/页面权限 ------------
+    菜单管理 ------------
     <div class="fle mb10">
-      <el-button
-        size="mini"
-        type="primary"
-        class="mr20"
-        @click="addOrEdit(1)"
-      >新增</el-button>
+      <el-button size="mini" type="primary" class="mr20" @click="addOrEdit(1)">新增</el-button>
     </div>
     <div class="content-box">
       <el-table
         :data="tableData"
-        style="width: 100%;margin-bottom: 20px;"
-        row-key="id"
-        align="center"
-        default-expand-all
+        border
+        row-key="path"
         :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
       >
-        <el-table-column
-          prop="meta.title"
-          label="菜单名称"
-          width="130px"
-        />
-        <el-table-column
-          prop="component"
-          label="前端组件"
-        />
-        <el-table-column
-          prop="path"
-          label="菜单路径"
-        />
-        <el-table-column
-          prop="name"
-          label="缓存名称"
-        />
-        <el-table-column
-          prop="hidden"
-          label="是否隐藏"
-          :formatter="formatter"
-        />
-        <el-table-column
-          prop="alwaysShow"
-          label="是否始终显示"
-          :formatter="formatter"
-        />
-        <el-table-column
-          prop="redirect"
-          label="重定向"
-        />
-        <el-table-column
-          prop="meta.icon"
-          label="菜单图标"
-        />
-        <el-table-column
-          prop="meta.roles"
-          label="按钮权限"
-        />
-        <el-table-column
-          prop="meta.noCache"
-          label="是否缓存"
-          :formatter="formatter"
-        />
-        <el-table-column
-          prop="meta.breadcrumb"
-          label="是否在面包屑中显示"
-          :formatter="formatter"
-        />
-        <el-table-column
-          prop="meta.activeMenu"
-          label="高亮显示项"
-        />
-        <el-table-column
-          prop="meta.affix"
-          label="固定标签"
-          :formatter="formatter"
-        />
-        <el-table-column
-          align="right"
-          label="操作"
-          width="150px"
-        >
+        <el-table-column align="left" prop="meta.title" label="菜单名称" />
+        <el-table-column align="center" prop="meta.icon" label="菜单图标" />
+        <el-table-column align="center" prop="path" label="菜单路径" />
+        <el-table-column align="center" prop="hidden" label="是否隐藏" :formatter="formatter" />
+        <el-table-column align="center" prop="alwaysShow" label="是否始终显示" :formatter="formatter" />
+        <el-table-column align="center" prop="meta.noCache" label="是否缓存" :formatter="formatter" />
+        <el-table-column align="center" prop="meta.breadcrumb" label="是否在面包屑中显示" :formatter="formatter" />
+        <el-table-column align="center" prop="meta.affix" label="固定标签" :formatter="formatter" />
+        <el-table-column align="center" label="操作" width="150">
           <template slot-scope="scope">
-            <el-button
-              size="mini"
-              @click="addOrEdit(2,scope.$index, scope.row)"
-            >编辑</el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
-            >删除</el-button>
+            <el-button size="mini" @click="addOrEdit(2,scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -104,6 +36,7 @@
       />
     </div>
     <drawer ref="drawer" />
+
   </div>
 </template>
 
@@ -117,7 +50,7 @@ export default {
     mixins: [search],
     data() {
         return {
-            tableData: menuList
+            tableData: {}
         }
     },
     watch: {
@@ -141,11 +74,7 @@ export default {
             this.tableData = menuList
         },
         formatter(row) {
-            if (row.state === true) {
-                return '是'
-            } else {
-                return '否'
-            }
+            return row.state ? '是' : '否'
         },
         addOrEdit(type, row) {
             this.$refs.drawer.openDrawer(type, row, this.tableData)
