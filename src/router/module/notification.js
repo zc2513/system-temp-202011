5ef3ce31d771c93c3593fae4a380ea13b9505176
zc2513 <yyzc2513@163.com>
2020-11-04 17:46:23 +0800

    .

--- src/router/module/notification.js
+++ src/router/module/notification.js
@@ -7,15 +7,21 @@ import Layout from '@/layout/tblyout.vue'
 const notification = {
     path: '/notification',
     component: Layout,
-    redirect: '/notification/index',
+    redirect: '/notification/lockover',
     meta: { title: '通知管理', icon: 'notification' },
     alwaysShow: false,
     children: [
         {
+            path: 'lockover',
+            name: 'Lockover',
+            component: () => import('@/views/notification/lockover'),
+            meta: { title: '消息查看', icon: '' }
+        },
+        {
             path: 'index',
             name: 'notification',
             component: () => import('@/views/notification/index'),
-            meta: { title: '通知管理', icon: '' }
+            meta: { title: '消息发布', icon: '' }
         },
         {
             path: 'info',
