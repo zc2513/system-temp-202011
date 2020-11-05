443720bb9ee30dd456c1bfd09b5c955e486c4de4
zc2513 <yyzc2513@163.com>
2020-11-05 14:55:57 +0800

    .

--- src/components/globalComponent/z-groud-person.vue
+++ src/components/globalComponent/z-groud-person.vue
@@ -34,12 +34,7 @@ export default {
     watch: {
         value: {
             handler(v) {
-                if (Array.isArray(v)) {
-                // 回显处理
-                } else {
-                    this.selDatas = v ? v.split(',') : []
-                    console.log(this.selDatas, '编辑')
-                }
+                // 回显暂无处理....
             },
             immediate: true
         }
@@ -49,7 +44,6 @@ export default {
     },
     methods: {
         init() {
-            // console.log(this.value)
             queryArearDeptGroupById({ userId: this.userInfo.id }).then(res => {
                 if (res.code === 200) {
                     this.options = res.result.map(item => {
@@ -61,26 +55,9 @@ export default {
                                     value: ele.deptId,
                                     label: ele.deptName,
                                     children: ele.groupList.map(e => {
-                                        if (this.value && this.value.length && this.value.includes(e.groupId)) {
-                                            console.log(this.value)
-                                            this.getPerson(e).then(res => {
-                                                return {
-                                                    value: e.groupId,
-                                                    label: e.groupName
-                                                }
-                                            })
-                                            // return this.getPerson(e)
-                                            // const arr = () => this.getPerson(e.groupId)
-                                            // console.log(arr, 99999999)
-                                            // return {
-                                            //     value: e.groupId,
-                                            //     label: e.groupName
-                                            // }
-                                        } else {
-                                            return {
-                                                value: e.groupId,
-                                                label: e.groupName
-                                            }
+                                        return {
+                                            value: e.groupId,
+                                            label: e.groupName
                                         }
                                     })
                                 }
@@ -112,25 +89,7 @@ export default {
             }
         },
         getkey(e) {
-            console.log(e, 6666666666666666666)
             this.$emit('input', e.join(','))
-        },
-        getPerson(e) {
-            return new Promise((resolve) => {
-                queryUserBaseByGroupId({ groupId: e.groupId }).then(res => {
-                    if (res.code === 200) {
-                        resolve(res.result.map(item => {
-                            return {
-                                value: item.id,
-                                label: item.realname,
-                                leaf: true
-                            }
-                        }))
-                    } else {
-                        this.$message.error(res.message || '未返回错误状态')
-                    }
-                })
-            })
         }
     }
 }
