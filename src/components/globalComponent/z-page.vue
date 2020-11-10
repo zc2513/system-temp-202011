f4e81fe18364e551098370866e7a48034770ff8f
闫斯敏 <ail>
2020-11-10 10:43:20 +0800

    .

--- src/components/globalComponent/z-page.vue
+++ src/components/globalComponent/z-page.vue
@@ -3,7 +3,7 @@
     <el-pagination
       :current-page="currentPage"
       :page-size="pageSize"
-      :page-sizes="[10,20,50,100,200,500,1000]"
+      :page-sizes="[5,10,15]"
       layout="total ,sizes,prev, pager, next, jumper"
       :total="total"
       @size-change="handleSizeChange"
